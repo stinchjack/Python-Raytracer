f0125b6526c341d1ec82eae958f69d9301ca590f
Jack Stinchcombe <stinchjack@myvuw.ac.nz>
2016-03-29 11:56:31 +1300

    Started doc strings for shape.py

--- shape.py
+++ shape.py
@@ -13,8 +13,29 @@ SHAPE_SPECULARCOLOUR_FUNC = 7
 SHAPE_TRANSFORM = 8
 SHAPE_DATA = 9
 
+"""Functions for dealing with shapes. A shape is a list with the following
+elements:
+
+* the string 'shape'
+* a string identifying the type of shape, e.g., 'disc'
+* a colour tuple to use for the diffuse colour of the shape, where no function
+  is used to calculate diffuse colour otherwise
+* a colour tuple to use for the specular colour of the shape, where no function
+  is used to calculate diffuse colour otherwise
+* a function to test the intersection of a ray with the shape
+* a function to test if a point is inside the shape
+* a function for getting the diffuse colour at a particluar intersection
+  point on the object
+* a function for getting the specular colour at a particluar intersection
+  point on the object
+* a Transformation for the shape
+* a dictionary of other shape data, as needed by particular shapes"""
+
 
 def shape_set_transform(shape, transform):
+    """Sets the transformation for a shape.
+    :param shape: a shape
+    :param transform: a Transformation object"""
     if isinstance(transform, Transform):
         shape[SHAPE_TRANSFORM] = transform
     elif type(transform) is dict:
@@ -40,6 +61,11 @@ def shape_specular_colour(shape, intersect_result):
 
 
 def shape_empty_shape():
+    """"Returns a list with some starting elements necessary for a shape.
+    
+    :return: ['shape', None, None, None, None, None, shape_diffuse_colour,
+             shape_specular_colour, None, {}]
+    """
     return ['shape', None, None, None, None, None, shape_diffuse_colour,
             shape_specular_colour, None, {}]
 
@@ -50,6 +76,11 @@ def shape_point_inside(shape, cartesian):
 
 
 def shape_test_intersect(shape, ray):
+    """Tests a ray for intersection against a shape.
+ 
+    :param shape: the shape to test against 
+    :param ray: the ray to test
+    :return :"""
     if shape[SHAPE_TRANSFORM] is not None:
         result = shape[SHAPE_INTERSECT_FUNC](
             shape, shape[SHAPE_TRANSFORM].transform(ray))
